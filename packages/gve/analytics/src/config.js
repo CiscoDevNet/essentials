5d56a7995d6caca41e938e4404264418c3bc6de5
Matt <matnorri@cisco.com>
2021-05-19 14:14:20 -0400

    refactor(@gve/analytics): add doc header for default analtyics URL
    affects: @gve/analytics

--- packages/gve/analytics/src/config.js
+++ packages/gve/analytics/src/config.js
@@ -1,5 +1,10 @@
+/**
+ * The default analytics URL
+ * @constant {String}
+ */
+const DEFAULT_ANALYTICS_URL = "https://api.amplitude.com/2/httpapi";
+
 module.exports = {
   ANALYTICS_API_KEY: process.env.ANALYTICS_API_KEY,
-  ANALYTICS_URL:
-    process.env.ANALYTICS_URL || "https://api.amplitude.com/2/httpapi",
+  ANALYTICS_URL: process.env.ANALYTICS_URL || DEFAULT_ANALYTICS_URL,
 };
