cc337bd5d79fab879a6bdad0b1f88fb1f173b8de
Matt <matnorri@cisco.com>
2021-07-29 10:59:30 -0400

    feat(Salesforce): add suggested, detailed message to SalesforceLoginError
    affects: @gve/salesforce

--- packages/gve/salesforce/src/errors/login-error.js
+++ packages/gve/salesforce/src/errors/login-error.js
@@ -1,8 +1,32 @@
 const SalesforceError = require("./error");
 
 class SalesforceLoginError extends SalesforceError {
-  constructor(message) {
-    super(message);
+  constructor(error) {
+    super(error?.message || error);
+    const { code: errorCode, errno: errorNumber, hostname: hostName } = error;
+    this.errorCode = errorCode;
+    this.errorNumber = errorNumber;
+    this.hostName = hostName;
+  }
+
+  get suggestedMessage() {
+    let solution;
+    if (this.errorCode === "ENOTFOUND") {
+      solution =
+        "The host address could not be found. Connect to VPN and try again.";
+    }
+
+    const messageParts = [
+      `Salesforce not connected. Tried to connect to ${
+        this.hostName || "an unknown host"
+      }.`,
+      `Returned error code ${this.errorNumber || "unknown"} - ${
+        this.errorCode || "unknown"
+      }.`,
+      solution,
+    ];
+
+    return messageParts.join(" ").trim();
   }
 }
 
