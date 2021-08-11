df9c4b9eaa77cd7770fcc7ef9c497e0e687a6144
Matt <matnorri@cisco.com>
2021-08-11 11:31:01 -0400

    feat(google): revise how google.auth requires credentials
    affects: @gve/bot-middleware, @gve/google

--- packages/gve/google/src/auth/config.js
+++ packages/gve/google/src/auth/config.js
@@ -1,15 +1,25 @@
-const {
-  env,
-  GOOGLE_APPLICATION_CREDENTIALS,
-  GOOGLE_EMAIL,
-  GOOGLE_KEY,
-  GOOGLE_PROJECT_ID,
-} = require("../config");
+const { env, GVE_GOOGLE_PROJECT_ID } = require("../config");
+
+const GOOGLE_APPLICATION_CREDENTIALS = env.get(
+  "GOOGLE_APPLICATION_CREDENTIALS"
+);
+
+let GVE_GOOGLE_EMAIL;
+let GVE_GOOGLE_KEY;
+
+getCredentials();
+
+function getCredentials() {
+  if (!GOOGLE_APPLICATION_CREDENTIALS) {
+    GVE_GOOGLE_EMAIL = env.require("GVE_GOOGLE_EMAIL");
+    GVE_GOOGLE_KEY = env.require("GVE_GOOGLE_KEY");
+  }
+}
 
 module.exports = {
   env,
   GOOGLE_APPLICATION_CREDENTIALS,
-  GOOGLE_EMAIL,
-  GOOGLE_KEY,
-  GOOGLE_PROJECT_ID,
+  GVE_GOOGLE_EMAIL,
+  GVE_GOOGLE_KEY,
+  GVE_GOOGLE_PROJECT_ID,
 };
