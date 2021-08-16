213692edbde7d4cb7640c3016f6b1d363fb5a6be
Matt <matnorri@cisco.com>
2021-08-16 11:05:45 -0400

    refactor(google): use built-in function to get Google project ID
    affects: @gve/google

--- packages/gve/google/src/config.js
+++ packages/gve/google/src/config.js
@@ -1,20 +1,15 @@
 const { Env } = require("@humanwhocodes/env");
 const env = new Env();
 
-const GOOGLE_CLOUD_PROJECT = getProjectId();
-
 /**
  * @see https://github.com/googleapis/google-auth-library-nodejs/blob/1ca3b733427d951ed624e1129fca510d84d5d0fe/src/auth/googleauth.ts#L646
- * @returns
  */
-function getProjectId() {
-  return (
-    env.require("GOOGLE_CLOUD_PROJECT") ||
-    env.require("GCLOUD_PROJECT") ||
-    env.require("gcloud_project") ||
-    env.require("google_cloud_project")
-  );
-}
+const GOOGLE_CLOUD_PROJECT = env.first([
+  "GOOGLE_CLOUD_PROJECT",
+  "GCLOUD_PROJECT",
+  "gcloud_project",
+  "google_cloud_project",
+]);
 
 module.exports = {
   env,
