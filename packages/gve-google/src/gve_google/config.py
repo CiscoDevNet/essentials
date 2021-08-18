960c85fa5291db6d6c3fdd391a48ca2a380976be
Matt <matnorri@cisco.com>
2021-08-18 15:10:18 -0400

    WIP(gve-google): create initial Python package

---
+++ packages/gve-google/src/gve_google/config.py
@@ -0,0 +1,5 @@
+from dotenv import load_dotenv
+import os.path
+
+
+load_dotenv()
---
+++ packages/gve-google/tests/test_gve_google.py
@@ -0,0 +1,8 @@
+from gve_google import __version__, config
+
+
+def test_version():
+    assert __version__ == "0.1.0"
+
+def test_config():
+    print('blah blah')
