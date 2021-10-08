ac9c79f9225acebde94264c6d1efcc44f90d646d
Matt <matnorri@cisco.com>
2021-10-08 14:28:48 -0400

    test(core): add tests for extracting a domain from a URL
    affects: @gve/core

--- packages/gve/core/src/index.js
+++ packages/gve/core/src/index.js
@@ -28,8 +28,8 @@ function getDomain(url) {
   let domain;
 
   try {
-    const normalizedUrl = _normalizeMalformedUrl(url);
-    ({ hostname: domain } = nodeUrl.parse(normalizedUrl));
+    const normalizedUrl = _normalizeMalformedUrl(url, false);
+    ({ hostname: domain } = new URL(normalizedUrl));
     debug(`domain: ${domain}: extracted from: "${url}"`);
   } catch (_) {
     debug(`domain: not extracted from: "${url}"`);
@@ -45,11 +45,12 @@ function getDomain(url) {
  * @returns {String} Normalized URL or empty string
  * @private
  */
-function _normalizeMalformedUrl(url) {
+function _normalizeMalformedUrl(url, shouldPreserveWww = true) {
   // Handle URLs starting with special characters from malformed sources,
   // e.g., "://www.cisco.com/c/en/us/products/.../datasheet-c78-741988.html"
   const sanitizedUrl = url.replace(/^([^a-zA-Z0-9])*/, "");
-  return normalizeUrl(sanitizedUrl, URL_OPTIONS);
+  const options = shouldPreserveWww ? URL_OPTIONS : undefined;
+  return normalizeUrl(sanitizedUrl, options);
 }
 
 module.exports = {
--- packages/gve/core/src/index.test.js
+++ packages/gve/core/src/index.test.js
@@ -1,27 +1,43 @@
-const { normalizeUrl } = require("./index");
+const { getDomain, normalizeUrl } = require("./index");
+
+const URL = "http://www.cisco.com/c/en/us/products/datasheet-c78-741988.html";
+const SECURE_URL =
+  "https://www.cisco.com/c/en/us/products/datasheet-c78-741988.html";
+const MALFORMED_URL =
+  "://www.cisco.com/c/en/us/products/datasheet-c78-741988.html";
 
 describe("normalizeUrl", () => {
   it("normalizes a URL", () => {
-    const url =
-      "http://www.cisco.com/c/en/us/products/datasheet-c78-741988.html";
-    let normalizedUrl = normalizeUrl(url);
+    const normalizedUrl = normalizeUrl(URL);
     expect(normalizedUrl).toEqual(
       "http://www.cisco.com/c/en/us/products/datasheet-c78-741988.html"
     );
   });
   it("normalizes a secure URL", () => {
-    const secureUrl =
-      "https://www.cisco.com/c/en/us/products/datasheet-c78-741988.html";
-    normalizedUrl = normalizeUrl(secureUrl);
+    const normalizedUrl = normalizeUrl(SECURE_URL);
     expect(normalizedUrl).toEqual(
       "https://www.cisco.com/c/en/us/products/datasheet-c78-741988.html"
     );
   });
   it("normalizes a malformed URL", () => {
-    const url = "://www.cisco.com/c/en/us/products/datasheet-c78-741988.html";
-    const normalizedUrl = normalizeUrl(url);
+    const normalizedUrl = normalizeUrl(MALFORMED_URL);
     expect(normalizedUrl).toEqual(
       "http://www.cisco.com/c/en/us/products/datasheet-c78-741988.html"
     );
+  });
+});
+
+describe("getDomain", () => {
+  it("extracts the domain from a URL", () => {
+    const domain = getDomain(URL);
+    expect(domain).toEqual("cisco.com");
+  });
+  it("extracts the domain from a secure URL", () => {
+    const domain = getDomain(SECURE_URL);
+    expect(domain).toEqual("cisco.com");
+  });
+  it("extracts the domain from a malformed URL", () => {
+    const domain = getDomain(MALFORMED_URL);
+    expect(domain).toEqual("cisco.com");
   });
 });
