971e2f6277d84191f5529ddd2dce01040cdb2607
Matt <matnorri@cisco.com>
2021-10-15 16:46:51 -0400

    WIP(docs): configure docs site

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,114 +1,103 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+const lunrSearch = require.resolve("docusaurus-lunr-search");
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
-(module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+(
+  module.exports = {
+    title: "CiscoDevNet Essentials",
+    tagline: "Packages to build, release, and run great software",
+    url: "https://github.com",
+    baseUrl: "/pages/CiscoDevNet/essentials/",
+    onBrokenLinks: "warn",
+    onBrokenMarkdownLinks: "throw",
+    favicon: "img/favicon.ico",
+    organizationName: "CiscoDevNet",
+    projectName: "essentials",
+    trailingSlash: true,
 
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+    plugins: [lunrSearch],
+
+    presets: [
+      [
+        "@docusaurus/preset-classic",
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            sidebarPath: require.resolve("./sidebars.js"),
+            editUrl:
+              "https://github.com/CiscoDevNet/essentials/edit/main/docs/",
+          },
+          blog: {
+            showReadingTime: true,
+            editUrl:
+              "https://github.com/CiscoDevNet/essentials/edit/main/docs/",
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
+        }),
+      ],
+    ],
+
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+        navbar: {
+          title: "CiscoDevNet Essentials",
+          logo: {
+            alt: "CiscoDevNet Essentials Logo",
+            src: "img/logo.svg",
+          },
+          items: [
+            {
+              type: "doc",
+              docId: "documentation",
+              position: "left",
+              label: "Documentation",
+            },
+            { to: "/blog", label: "Blog", position: "left" },
+            {
+              href: "https://github.com/CiscoDevNet/essentials",
+              label: "GitHub",
+              position: "right",
+            },
+          ],
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+        footer: {
+          style: "dark",
+          links: [
+            {
+              title: "Community",
+              items: [
+                {
+                  label: "Twitter",
+                  href: "https://twitter.com/CiscoDevNet",
+                },
+              ],
+            },
+            {
+              title: "More",
+              items: [
+                {
+                  label: "Blog",
+                  to: "/blog",
+                },
+                {
+                  label: "GitHub",
+                  href: "https://github.com/CiscoDevNet/essentials",
+                },
+              ],
+            },
+          ],
+          copyright: `Copyright ${new Date().getFullYear()} Cisco Systems, Inc. or its Affiliates`,
         },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+        prism: {
+          theme: lightCodeTheme,
+          darkTheme: darkCodeTheme,
         },
       }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
-});
+  }
+);
