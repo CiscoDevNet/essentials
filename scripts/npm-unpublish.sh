1eb21f01ea5508b48b1c802eb712e66239884466
Matt <matnorri@cisco.com>
2021-12-17 10:07:31 -0500

    chore(github): revise workflows to version and publish packages
    affects: @gve/analytics, @gve/bot-commands, @gve/bot-factory, @gve/bot-middleware, @gve/bridge,
    @gve/cards, @gve/core, @gve/google, @gve/releases, @gve/salesforce

---
+++ scripts/npm-unpublish.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+# description: Unpublish @gve packages
+# author: Matt Norris <matnorri@cisco.com>
+# see: https://stackoverflow.com/a/61231856/154065
+
+npm unpublish @gve/analytics --force
+npm unpublish @gve/bot-commands --force
+npm unpublish @gve/bot-factory --force
+npm unpublish @gve/bot-middleware --force
+npm unpublish @gve/bridge --force
+npm unpublish @gve/cards --force
+npm unpublish @gve/core --force
+npm unpublish @gve/google --force
+npm unpublish @gve/releases --force
+npm unpublish @gve/salesforce --force
