38b8bab522f8a3a1b9583754d2756b7b162f5fd5
Matt <matnorri@cisco.com>
2022-03-25 14:17:27 -0400

    feat(@cisco/docs-docusaurus): add package for working with Docusaurus
    affects: @cisco/digital-garden, @cisco/docs-docusaurus, @gve/analytics, @gve/bot-commands,
    @gve/bot-factory, @gve/bot-middleware, @gve/bridge, @gve/cards, @gve/core, @gve/google,
    @gve/releases, @gve/salesforce

---
+++ packages/cisco/digital-garden/config/docusaurus.js
@@ -0,0 +1,22 @@
+const Docusaurus = require("@cisco/docs-docusaurus");
+const env = require("./env");
+
+const organizationName = env.require("CONFIG_SITE_REPO_ORG_NAME");
+const projectName = env.require("CONFIG_SITE_REPO_PROJECT_NAME");
+const url = env.require("CONFIG_SITE_URL");
+
+const title = env.get("CONFIG_SITE_TITLE", "My Site");
+const tagline = env.get("CONFIG_SITE_TAGLINE", "");
+
+const repoBranch = env.get("CONFIG_SITE_REPO_BRANCH");
+const repoDirPath = env.get("CONFIG_SITE_REPO_DIR_PATH");
+
+const additionalConfig = { title, tagline, repoBranch, repoDirPath };
+let docusaurus = new Docusaurus(
+  organizationName,
+  projectName,
+  url,
+  additionalConfig
+);
+
+module.exports = docusaurus;
---
+++ packages/cisco/digital-garden/custom.config.js
@@ -0,0 +1,101 @@
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+const { docusaurus } = require("./config");
+
+const { basicConfig } = docusaurus;
+
+const { title, repoPath, repoTreePath } = docusaurus;
+
+basicConfig.presets = [
+  [
+    "classic",
+    /** @type {import('@docusaurus/preset-classic').Options} */
+    ({
+      docs: {
+        sidebarPath: require.resolve("./sidebars.js"),
+        editUrl: repoTreePath,
+      },
+      blog: {
+        showReadingTime: true,
+        editUrl: repoTreePath,
+      },
+      theme: {
+        customCss: require.resolve("./src/css/custom.css"),
+      },
+    }),
+  ],
+];
+
+basicConfig.themeConfig =
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  {
+    navbar: {
+      title,
+      logo: {
+        alt: `${title} Logo`,
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "doc",
+          docId: "intro",
+          position: "left",
+          label: "Tutorial",
+        },
+        { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: repoPath,
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: repoPath,
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  };
+
+module.exports = basicConfig;
