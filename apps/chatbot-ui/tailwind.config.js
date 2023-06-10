5dac68817fcaec560af9e68a6dfb98edc6295843
Matt <matnorri@cisco.com>
2023-06-09 23:36:50 -0400

    feat(chatbot-ui): add chatbot-ui

---
+++ apps/chatbot-ui/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        // 'tuna': '#343541',
+        tuna: 'red',
+        'sidebar-highlighted': '#343541',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      visibility: ['group-hover'],
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
